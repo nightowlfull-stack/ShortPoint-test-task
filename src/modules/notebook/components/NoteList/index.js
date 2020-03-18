d7e3f3681338c519fb67e8245ba11e9935618ca2
Nightowl <nightowl@quicklancers.me>
2020-03-18 18:34:18 +0530

    feat: add noteList react ui

--- src/modules/notebook/components/NoteList/index.js
+++ src/modules/notebook/components/NoteList/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import './notelist.css'
+import Note from '../Note'
 
-const AddNoteForm = () => {
+const NoteList = ({ notes, startNoteEditing, deleteNote }) => {
   return (
-    <div>
-      NOTELIST
+    <div class="card-block">
+      <div class="row">
+        {notes
+          .map(note => (
+            <Note
+              key={note.id}
+              note={note}
+              editNote={() => startNoteEditing(note.id)}
+              deleteNote={() => deleteNote([note.id])}
+            />
+          ))}
+      </div>
     </div>
   );
 }
 
-export default AddNoteForm;
+export default NoteList;
