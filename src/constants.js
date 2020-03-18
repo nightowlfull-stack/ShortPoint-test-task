415b3706c896e881e5bc14a8be44a7fb46034ce6
Nightowl <nightowl@quicklancers.me>
2020-03-18 18:14:19 +0530

    feat: add color options to constant file

---
+++ src/constants.js
@@ -0,0 +1,26 @@
+export const COLOR_OPTIONS = [
+  {
+    value: 'red',
+    label: 'Red',
+  },
+  {
+    value: 'blue',
+    label: 'Blue',
+  },
+  {
+    value: 'purple',
+    label: 'Purple',
+  },
+  {
+    value: 'green',
+    label: 'Green',
+  },
+  {
+    value: 'lightgreen',
+    label: 'Light Green',
+  },
+  {
+    value: 'orange',
+    label: 'Orange',
+  },
+]
