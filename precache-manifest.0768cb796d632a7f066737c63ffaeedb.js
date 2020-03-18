f072812d9b26d5ca72b9cfad89b896b5cfa0a7d6
Nightowl <nightowl@quicklancers.me>
2020-03-18 18:46:32 +0530

    Updates

---
+++ precache-manifest.0768cb796d632a7f066737c63ffaeedb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9953cc3b13d7f953595f0df31be246f",
+    "url": "/ShortPoint-test-task/index.html"
+  },
+  {
+    "revision": "af4beb795bb95bb1d01b",
+    "url": "/ShortPoint-test-task/static/css/main.bbd29fba.chunk.css"
+  },
+  {
+    "revision": "7c7549a2cc66be1c0f2b",
+    "url": "/ShortPoint-test-task/static/js/2.bd149ac9.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/ShortPoint-test-task/static/js/2.bd149ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af4beb795bb95bb1d01b",
+    "url": "/ShortPoint-test-task/static/js/main.f97c3dca.chunk.js"
+  },
+  {
+    "revision": "9725f8f6f800d8d13f05",
+    "url": "/ShortPoint-test-task/static/js/runtime-main.845760d7.js"
+  }
+]);
